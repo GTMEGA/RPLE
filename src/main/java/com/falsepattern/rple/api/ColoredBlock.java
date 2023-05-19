c59d6eece5b037f3826c5de568ae2cfe478d903a
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2023-05-19 13:39:41 +0200

    super call helper logic

--- src/main/java/com/falsepattern/rple/api/ColoredBlock.java
+++ src/main/java/com/falsepattern/rple/api/ColoredBlock.java
@@ -9,25 +9,92 @@
 package com.falsepattern.rple.api;
 
 import net.minecraft.block.Block;
+import lombok.SneakyThrows;
+import lombok.var;
+
 import net.minecraft.world.IBlockAccess;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+
 
 /**
  * This class is implicitly implemented on {@link Block} using a mixin. You can safely cast any class that extends Block
  * to this interface.
- * @apiNote Only override these methods if you know what you are doing! The default implementation is good enough for
- * 99% of cases. The defaults are here so that you don't accidentally override them.
+ * Note: Due to mixin breaking super calls with mixin-implemented interfaces on super classes,
+ * you need to use {@link SuperCallHelper}.
  */
 public interface ColoredBlock {
+    @SneakyThrows
     default int getColoredLightValue(IBlockAccess world, int meta, int colorChannel, int x, int y, int z) {
         return 0;
     }
+    @SneakyThrows
     default int getColoredLightOpacity(IBlockAccess world, int meta, int colorChannel, int x, int y, int z) {
         return 0;
     }
+    @SneakyThrows
     default void setColoredLightValue(int meta, int r, int g, int b) {
-
     }
+    @SneakyThrows
     default void setColoredLightOpacity(int meta, int r, int g, int b) {
+    }
 
+    class SuperCallHelper {
+        private static final MethodType getColoredLightValueM;
+        private static final MethodType getColoredLightOpacityM;
+        private static final MethodType setColoredLightValueM;
+        private static final MethodType setColoredLightOpacityM;
+
+        static {
+            try {
+                getColoredLightValueM = getMType(ColoredBlock.class.getDeclaredMethod("getColoredLightValue", IBlockAccess.class, int.class, int.class, int.class, int.class, int.class));
+                getColoredLightOpacityM = getMType(ColoredBlock.class.getDeclaredMethod("getColoredLightOpacity", IBlockAccess.class, int.class, int.class, int.class, int.class, int.class));
+                setColoredLightValueM = getMType(ColoredBlock.class.getDeclaredMethod("setColoredLightValue", int.class, int.class, int.class, int.class));
+                setColoredLightOpacityM = getMType(ColoredBlock.class.getDeclaredMethod("setColoredLightOpacity", int.class, int.class, int.class, int.class));
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private static MethodType getMType(Method method) {
+            return MethodType.methodType(method.getReturnType(), method.getParameterTypes());
+        }
+
+        @SneakyThrows
+        public static int getColoredLightValue(MethodHandles.Lookup lookup, Class<?> relativeTo, Object instance, IBlockAccess world, int meta, int colorChannel, int x, int y, int z) {
+            return (int) doSuperCall(lookup, relativeTo, "getColoredLightValue", getColoredLightValueM).invoke(instance, world, meta, colorChannel, x, y, z);
+        }
+        @SneakyThrows
+        public static int getColoredLightOpacity(MethodHandles.Lookup lookup, Class<?> relativeTo, Object instance, IBlockAccess world, int meta, int colorChannel, int x, int y, int z) {
+            return (int) doSuperCall(lookup, relativeTo, "getColoredLightOpacity", getColoredLightOpacityM).invoke(instance, world, meta, colorChannel, x, y, z);
+        }
+        @SneakyThrows
+        public static void setColoredLightValue(MethodHandles.Lookup lookup, Class<?> relativeTo, Object instance, int meta, int r, int g, int b) {
+            doSuperCall(lookup, relativeTo, "setColoredLightValue", setColoredLightValueM).invoke(instance, meta, r, g, b);
+        }
+        @SneakyThrows
+        public static void setColoredLightOpacity(MethodHandles.Lookup lookup, Class<?> relativeTo, Object instance, int meta, int r, int g, int b) {
+            doSuperCall(lookup, relativeTo, "setColoredLightOpacity", setColoredLightOpacityM).invoke(instance, meta, r, g, b);
+        }
+
+        private static MethodHandle doSuperCall(MethodHandles.Lookup lookup, Class<?> relativeTo, String name, MethodType type) {
+            var klass = relativeTo.getSuperclass();
+            while (klass != null) {
+                try {
+                    var handle = lookup.findSpecial(klass, name, type, relativeTo);
+                    return handle;
+                } catch (NoSuchMethodException | IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (
+                        Throwable e) {
+                    throw new RuntimeException(e);
+                }
+                klass = klass.getSuperclass();
+            }
+            throw new RuntimeException("Could not super-call " + name + " with handle " + type);
+        }
     }
 }
