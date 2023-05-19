8c3da6b8918e495c0b1721818269a1c7ff47b0a0
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2023-05-19 13:43:34 +0200

    Revert "chisel in dev"
    This reverts commit 6aff23aa

--- src/main/java/com/falsepattern/rple/internal/mixin/plugin/TargetedMod.java
+++ src/main/java/com/falsepattern/rple/internal/mixin/plugin/TargetedMod.java
@@ -19,7 +19,7 @@ import static com.falsepattern.lib.mixin.ITargetedMod.PredicateHelpers.startsWit
 @RequiredArgsConstructor
 public enum TargetedMod implements ITargetedMod {
     OPTIFINE("OptiFine", false, startsWith("optifine")),
-    CHISEL("Chisel", true, startsWith("chisel")),
+    CHISEL("Chisel", false, startsWith("chisel")),
     PROJECTRED_CORE("ProjectRed Core", false, startsWith("projectred").and(contains("base"))),
     PROJECTRED_ILLUMINATION("ProjectRed Illumination", false, startsWith("projectred").and(contains("lighting"))),
     MRTJPCORE("MRTJPCore", false, startsWith("mrtjpcore")),
