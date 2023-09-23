d8b2c52443f9cc07fb6bae9e55b3364a3039d3f9
Ven <tudurap.com@gmail.com>
2023-09-23 02:38:47 +0100

    Preparing for integrating ChunkCache

--- src/main/java/com/falsepattern/rple/api/common/RPLEWorldUtil.java
+++ src/main/java/com/falsepattern/rple/api/common/RPLEWorldUtil.java
@@ -7,8 +7,8 @@
 
 package com.falsepattern.rple.api.common;
 
-import com.falsepattern.lumina.api.lighting.LightType;
 import com.falsepattern.rple.api.common.color.ColorChannel;
+import com.falsepattern.rple.internal.client.render.LightValueOverlayRenderer;
 import lombok.val;
 import net.minecraft.world.IBlockAccess;
 
@@ -16,32 +16,12 @@ import static com.falsepattern.rple.api.common.RPLEColorUtil.*;
 import static com.falsepattern.rple.api.common.color.ColorChannel.*;
 import static com.falsepattern.rple.internal.common.world.RPLEWorldRoot.rple$getWorldRootFromBlockAccess;
 
+/**
+ * Currently only used by {@link LightValueOverlayRenderer}
+ */
 public final class RPLEWorldUtil {
     private RPLEWorldUtil() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
-    }
-
-    public static int getChannelLightValue(IBlockAccess world,
-                                           ColorChannel channel,
-                                           int posX,
-                                           int posY,
-                                           int posZ) {
-        val worldRoot = rple$getWorldRootFromBlockAccess(world);
-        if (worldRoot == null)
-            return channel.componentFromColor(errorColor());
-        return worldRoot.rple$world(channel).lumi$getLightValue(posX, posY, posZ);
-    }
-
-    public static int getChannelLightValue(IBlockAccess world,
-                                           ColorChannel channel,
-                                           LightType lightType,
-                                           int posX,
-                                           int posY,
-                                           int posZ) {
-        val worldRoot = rple$getWorldRootFromBlockAccess(world);
-        if (worldRoot == null)
-            return channel.componentFromColor(errorColor());
-        return worldRoot.rple$world(channel).lumi$getLightValue(lightType, posX, posY, posZ);
     }
 
     public static int getChannelBlockLightValue(IBlockAccess world,
@@ -76,20 +56,6 @@ public final class RPLEWorldUtil {
         return maxColorComponent(redLightValue, greenLightValue, blueLightValue);
     }
 
-    public static int getGreyscaleLightValue(IBlockAccess world,
-                                             LightType lightType,
-                                             int posX,
-                                             int posY,
-                                             int posZ) {
-        val worldRoot = rple$getWorldRootFromBlockAccess(world);
-        if (worldRoot == null)
-            return COLOR_MIN;
-
-        val redLightValue = worldRoot.rple$world(RED_CHANNEL).lumi$getLightValue(lightType, posX, posY, posZ);
-        val greenLightValue = worldRoot.rple$world(GREEN_CHANNEL).lumi$getLightValue(lightType, posX, posY, posZ);
-        val blueLightValue = worldRoot.rple$world(BLUE_CHANNEL).lumi$getLightValue(lightType, posX, posY, posZ);
-        return maxColorComponent(redLightValue, greenLightValue, blueLightValue);
-    }
 
     public static int getGreyscaleBlockLightValue(IBlockAccess world,
                                                   int posX,
