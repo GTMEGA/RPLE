a266442f9410ab9f066c7f454920716f0588fdc4
Ven <tudurap.com@gmail.com>
2023-10-08 12:30:48 +0100

    Extended the fix to name tags

--- src/main/java/com/falsepattern/rple/internal/mixin/mixins/client/RenderMixin.java
+++ src/main/java/com/falsepattern/rple/internal/mixin/mixins/client/RenderMixin.java
@@ -3,15 +3,16 @@ package com.falsepattern.rple.internal.mixin.mixins.client;
 import com.falsepattern.rple.internal.Compat;
 import com.falsepattern.rple.internal.mixin.helper.ShaderRenderHelper;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(RendererLivingEntity.class)
-public abstract class RendererLivingEntityMixin extends Render {
-    @Redirect(method = "passSpecialRender",
+@Mixin(Render.class)
+public abstract class RenderMixin {
+    @Redirect(method = "func_147906_a",
               at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL11;glDisable(I)V"),
               require = 1)
     private void disableTexture(int cap) {
@@ -20,12 +21,18 @@ public abstract class RendererLivingEntityMixin extends Render {
             ShaderRenderHelper.disableTexturing();
     }
 
-    @Redirect(method = "passSpecialRender",
+    @Redirect(method = "func_147906_a",
               at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL11;glEnable(I)V"),
               require = 1)
     private void enableTexture(int cap) {
         GL11.glEnable(cap);
         if (cap == GL11.GL_TEXTURE_2D && Compat.shadersEnabled())
             ShaderRenderHelper.enableTexturing();
+    }
+
+    @ModifyConstant(method = "func_147906_a",
+                    constant = @Constant(intValue = 553648127))
+    private static int strideSizeInts(int constant) {
+        return -1;
     }
 }
