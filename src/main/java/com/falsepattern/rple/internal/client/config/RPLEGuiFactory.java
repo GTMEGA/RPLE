1051a2eb2b7f9866c3f006ed9f2dba68abb05c0a
FalsePattern <me@falsepattern.com>
2024-06-25 00:43:23 +0200

    this mod is now FOSS

--- src/main/java/com/falsepattern/rple/internal/client/config/RPLEGuiFactory.java
+++ src/main/java/com/falsepattern/rple/internal/client/config/RPLEGuiFactory.java
@@ -1,3 +1,29 @@
+/*
+ * Right Proper Lighting Engine
+ *
+ * Copyright (C) 2023-2024 FalsePattern, Ven
+ * All Rights Reserved
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This program comes with additional permissions according to Section 7 of the
+ * GNU Affero General Public License. See the full LICENSE file for details.
+ */
+
 package com.falsepattern.rple.internal.client.config;
 
 import com.falsepattern.lib.config.ConfigException;
--- src/main/java/com/falsepattern/rple/internal/common/block/RPLEBlockInit.java
+++ src/main/java/com/falsepattern/rple/internal/common/block/RPLEBlockInit.java
@@ -1,8 +1,27 @@
 /*
- * Copyright (c) 2023 FalsePattern, Ven
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
- * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ * Right Proper Lighting Engine
+ *
+ * Copyright (C) 2023-2024 FalsePattern, Ven
+ * All Rights Reserved
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This program comes with additional permissions according to Section 7 of the
+ * GNU Affero General Public License. See the full LICENSE file for details.
  */
 
 package com.falsepattern.rple.internal.common.block;
