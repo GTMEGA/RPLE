be10c9293465d4fe7822b52397d8f44bb3f53d1d
FalsePattern <me@falsepattern.com>
2025-10-14 19:29:56 +0200

    build: update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,12 +67,12 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.ventooth:swansong-mc1.7.10:1.2.0:dev")
+    compileOnly("com.ventooth:swansong-mc1.7.10:1.2.1:dev")
     compileOnly("org.joml:joml:1.10.8")
 
     apiSplit("com.falsepattern:lumi-mc1.7.10:1.2.0")
-    implementationSplit("com.falsepattern:falsepatternlib-mc1.7.10:1.9.0")
-    implementation("com.falsepattern:falsetweaks-mc1.7.10:4.1.0:dev")
+    implementationSplit("com.falsepattern:falsepatternlib-mc1.7.10:1.9.1")
+    implementation("com.falsepattern:falsetweaks-mc1.7.10:4.1.2:dev")
 
     val beddiumVersion = "1.0.0"
     val beddiumVersionJ21 = "$beddiumVersion-j21"
--- src/main/java/com/falsepattern/rple/internal/RightProperLightingEngine.java
+++ src/main/java/com/falsepattern/rple/internal/RightProperLightingEngine.java
@@ -46,8 +46,8 @@ import static com.falsepattern.rple.internal.Tags.*;
      acceptedMinecraftVersions = "[1.7.10]",
      guiFactory = GROUP_NAME + ".internal.client.config.RPLEGuiFactory",
      dependencies = "required-after:lumi@[1.2.0,);" +
-             "after:falsetweaks@[4.1.0,);" + // Hard dep, but only on clientside!
-             "required-after:falsepatternlib@[1.9.0,);")
+             "after:falsetweaks@[4.1.2,);" + // Hard dep, but only on clientside!
+             "required-after:falsepatternlib@[1.9.1,);")
 @NoArgsConstructor
 public final class RightProperLightingEngine {
     @SidedProxy(clientSide = GROUP_NAME + ".internal.proxy.ClientProxy",
